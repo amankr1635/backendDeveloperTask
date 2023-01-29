e78c248a43c14a25f6cd459f1da3a1bcc0a0902e
bhavi321 <bhavialkhaniya5@gmail.com>
2023-01-30 00:31:01 +0530

    b

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -10,12 +10,3 @@ const createCourse=async function(req,res){
 
 }
 
-
-
-const createCourse=async function(req,res){
-    let data=req.body;
-    let {title,description,videoUrl, topics, duration, category}=data
-
-    const createData=await courseModel.create(data)
-
-}
